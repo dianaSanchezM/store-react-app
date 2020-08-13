99100107f19767481152c1d5cf1d8b6b51056b46
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-08-13 09:40:25 -0500

    fetch funtions done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+  const getData = (path) => {
+    const url = `https://coding-challenge-api.aerolab.co/${path}`;
+      fetch(url, {
+        method: 'GET', 
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjM1NDJmYTc1MTI2YzAwNmQwOGJiMDYiLCJpYXQiOjE1OTczMjYwNzR9.31SDbCf2qkqMAB0ZENf7K8Z87jmNyK11_ODaHVYTl2c'
+        }
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+  };
+
+  const postData = (path, data) => {
+    fetch(`https://coding-challenge-api.aerolab.co/${path}`, {
+      method: 'POST', 
+      body: JSON.stringify(data), 
+      headers:{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjM1NDJmYTc1MTI2YzAwNmQwOGJiMDYiLCJpYXQiOjE1OTczMjYwNzR9.31SDbCf2qkqMAB0ZENf7K8Z87jmNyK11_ODaHVYTl2c'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
+  getData('user/me');
+  postData('user/points', {amount: 1000});
+  return <div>Hello world</div>;
 }
 
 export default App;
