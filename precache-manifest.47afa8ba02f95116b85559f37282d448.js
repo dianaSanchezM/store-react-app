af5724f86685607f0b6078bd6fdebc77fb2b887c
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-08-12 20:14:54 -0500

    Updates

---
+++ precache-manifest.47afa8ba02f95116b85559f37282d448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede37ccc8dbbf2cae0c6743cd5a212bf",
+    "url": "/store-react-app/index.html"
+  },
+  {
+    "revision": "817ed33cef55e9c84eec",
+    "url": "/store-react-app/static/js/2.5fcfa28a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-react-app/static/js/2.5fcfa28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1383e33617684ae32ca",
+    "url": "/store-react-app/static/js/main.e07e43ff.chunk.js"
+  },
+  {
+    "revision": "01a0624eb5d99b5ca156",
+    "url": "/store-react-app/static/js/runtime-main.c5d0caaf.js"
+  }
+]);
