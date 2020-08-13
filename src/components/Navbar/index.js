2bf67a96e47ac6686eb2175d4ff923ce151b213c
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-08-13 12:45:14 -0500

    react router added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./navbar.css";
 import Logo from "../../images/aerolab-logo.svg";
 import coin from "../../images/coin.svg";
+import { Link } from 'react-router-dom';
 
 const NavBar = ({ name, points }) => {
   return (
@@ -9,12 +10,23 @@ const NavBar = ({ name, points }) => {
       <div className="logo">
         <img src={Logo} alt="Logo" />
       </div>
+      <ul className="links">
+        <li >
+          <Link className="link" to='/profile'>
+            Profile
+          </Link>
+        </li>
+        <li >
+          <Link className="link" to='/'>
+            Products
+          </Link>
+        </li>
+      </ul>
       <div className="profile">
         <p>{name}</p>
         <div className="points">
           <p>{points}</p>
-            <img src={coin} alt="coin" />
-          
+          <img src={coin} alt="coin" />
         </div>
       </div>
     </div>
