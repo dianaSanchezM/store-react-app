593f32c20a18c5a63da60d189a6b46ac1d7b5eae
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-08-24 11:30:51 -0500

    Product card hover done

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -4,54 +4,55 @@ import BuyBlueIcon from "../../images/buy-blue.svg";
 import BuyWHiteIcon from "../../images/buy-white.svg";
 import coin from "../../images/coin.svg";
 import { AppContext } from "../../contexts/UserContext";
-import { fetchData, pathsData } from '../../const';
+import { fetchData, pathsData } from "../../const";
 
 const ProductCard = ({ _id, name, cost, category, img: { url } }) => {
   const { user, setUser } = useContext(AppContext);
 
   const handleRedeem = () => {
-    fetchData({ data: pathsData.redeem, body: { "productId": _id } }).then(res => fetchData({ data: pathsData.load_user}).then(result => setUser(result)));
-    
-  }
- 
+    fetchData({
+      data: pathsData.redeem,
+      body: { productId: _id },
+    }).then((res) =>
+      fetchData({ data: pathsData.load_user }).then((result) => setUser(result))
+    );
+  };
+
   const difference = user.points - cost;
   return (
     <div className="card">
-      {/* <div className="card-img">
-        {difference > 0 ? (
-          <img className="icon" src={BuyBlueIcon} alt="icon"></img>
-        ) : (
-          <div className="icon">
-            <div className="you-need">
-              <p> You need {-difference}</p>
-              <img src={coin} alt="coin" />
-            </div>
+      {difference > 0 ? (
+        <img className="icon" src={BuyBlueIcon} alt="icon"></img>
+      ) : (
+        <div className="icon">
+          <div className="you-need">
+            <p> You need {-difference}</p>
+            <img src={coin} alt="coin" />
           </div>
-        )}
-        <img className="product" src={url} alt="product"></img>
-      </div>
+        </div>
+      )}
+      <img className="product" src={url} alt="product"></img>
 
       <div className="info">
         <div className="line"></div>
         <p>{category}</p>
         <h3>{name}</h3>
-        <div className="points-card">
-          <p>{cost}</p>
-          <img src={coin} alt="coin" />
-        </div>
-      </div> */}
-      <div className="card-img">
-        <img className="icon" src={BuyWHiteIcon} alt="icon"></img>
       </div>
-      <div className="buy-info">
-        <div className="points-card">
-          <p>{cost}</p>
-          <img src={coin} alt="coin" />
+
+      {difference > 0 && (
+        <div className="card-hover">
+          <img className="icon" src={BuyWHiteIcon} alt="icon"></img>
+          <div className="buy-info">
+            <div className="points-card">
+              <p>{cost}</p>
+              <img src={coin} alt="coin" />
+            </div>
+            <button className="btn btn-add" onClick={() => handleRedeem()}>
+              Redeem now
+            </button>
+          </div>
         </div>
-        <button className='btn btn-add' onClick={()=>handleRedeem()}>
-          Redeem now
-        </button>
-      </div>
+      )}
     </div>
   );
 };
